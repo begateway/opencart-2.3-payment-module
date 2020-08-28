8ad532c0c6267078743d1d0f7e97da976adac680
avsh <avsh@users.noreply.github.com>
2020-08-28 19:01:06 +0300

    change payment option name

--- upload/catalog/language/en-gb/extension/payment/begateway.php
+++ upload/catalog/language/en-gb/extension/payment/begateway.php
@@ -1,5 +1,5 @@
 <?php
-$_['text_title']  = 'Pay by credit card online';
+$_['text_title']  = 'Pay online';
 $_['token_error'] = 'Error to create a payment token. Contact the store owner';
 $_['text_order']  = 'Order #';
 ?>
